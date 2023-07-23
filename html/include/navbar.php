07dcb9478614c64be8ba19c5bb29361fcc1591cc
Bruno <91850710+CS8ABG@users.noreply.github.com>
2023-07-23 01:09:54 +0000

    version change

--- html/include/navbar.php
+++ html/include/navbar.php
@@ -66,6 +66,7 @@
                 <option value="es">ES</option>
                 <option value="fr">FR</option>
                 <option value="it">IT</option>
+                <option value="nl">NL</option>
                 <option value="de">DE</option>
             </select>
             <li class="nav-item">
