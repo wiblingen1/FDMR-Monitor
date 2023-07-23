b661ae9f892db52889c80d3449248a77eafd1c3c
Bruno <91850710+CS8ABG@users.noreply.github.com>
2023-07-23 01:13:04 +0000

    dark/light mode issues

--- html/include/monitor.php
+++ html/include/monitor.php
@@ -1,11 +1,11 @@
 <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Monitor</h3>
+        <div class="card" style="background-color: #343a40;">
+          <div class="card-header ">
+            <h3 class="card-title " style="color: white;">Monitor</h3>
           </div>
-          <div class="card-body">
+          <div class="card-body text-bg-dark">
             <pre id="log" style="height: 40em; text-align: left; overflow-y: scroll; color: greenyellow;"></pre>
           </div>
         </div>
