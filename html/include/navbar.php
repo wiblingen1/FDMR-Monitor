c2db4374583be37b3f7f1d68a01be2d9e97f14c4
Bruno <91850710+CS8ABG@users.noreply.github.com>
2023-07-23 01:25:44 +0000

    added NL translations thanks to Paul ON6DP / OP0P

--- html/include/navbar.php
+++ html/include/navbar.php
@@ -1,7 +1,7 @@
 <nav class="main-header navbar navbar-expand-md navbar-light navbar-dark text-sm">
     <div class="container">
         <a href="#" class="navbar-brand">
-            <img src="img/Logo_mini.png" alt="FreeDMR Portugal" class="brand-image img-circle elevation-3"
+            <img src="img/Logo_mini.png" alt="FreeDMR" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
             <span class="brand-text font-weight-light">FreeDMR</span>
         </a>
@@ -12,37 +12,43 @@
         <div class="collapse navbar-collapse order-3" id="navbarCollapse">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="?p=home" class="nav-link" id="nav_dash"></a>
+                    <a href="index.php?p=home" class="nav-link" id="nav_dash"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="?p=systems" class="nav-link" id="nav_lnksys"></a>
+                    <a href="index.php?p=systems" class="nav-link" id="nav_lnksys"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="?p=systemstg" class="nav-link" id="nav_statg"></a>
+                    <a href="index.php?p=systemstg" class="nav-link" id="nav_statg"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="?p=openbridge" class="nav-link" id="nav_opb"></a>
+                    <a href="index.php?p=openbridge" class="nav-link" id="nav_opb"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="?p=toptg" class="nav-link" id="nav_tptg"></a>
+                    <a href="index.php?p=toptg" class="nav-link" id="nav_tptg"></a>
                 </li>
                 <li class="nav-item">
-                    <a href="?p=monitor" class="nav-link" id="nav_mon"></a>
+                    <a href="index.php?p=monitor" class="nav-link" id="nav_mon"></a>
                 </li>
+                <!-- <li class="nav-item">
+                    <a href="sslogin.php" class="nav-link">Self Service</a>
+                </li> -->
                 <li class="nav-item dropdown dropdown-hover">
                     <a id="dropdownSubMenu1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false" class="nav-link dropdown-toggle"><span
                             id="nav_info"></span></a>
                     <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
                         <li>
-                            <a href="?p=calc" class="dropdown-item" id="nav_calc"></a>
+                            <a href="index.php?p=calc" class="dropdown-item" id="nav_calc"></a>
                         </li>
                         <li>
-                            <a href="?p=wwtg" class="dropdown-item" id="nav_tglst"></a>
+                            <a href="index.php?p=wwtg" class="dropdown-item" id="nav_tglst"></a>
                         </li>
                         <li>
-                            <a href="?p=wwbridges" class="dropdown-item" id="nav_brdglst"></a>
+                            <a href="index.php?p=wwbridges" class="dropdown-item" id="nav_brdglst"></a>
                         </li>
+                        <!-- <li>
+                            <a href="index.php?p=wwservers" class="dropdown-item" id="nav_srvlst"></a>
+                        </li> -->
                     </ul>
                 </li>
             </ul>
